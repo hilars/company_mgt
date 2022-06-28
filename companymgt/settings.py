159ec21af960d23cf67acf06eb005b760745efab
Hilars <hilars@Hilarss-MacBook-Pro.local>
2022-06-28 09:39:57 +0300

    settings file updated

--- companymgt/settings.py
+++ companymgt/settings.py
@@ -67,7 +67,7 @@ TEMPLATE_DIRS = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'humancapital/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
